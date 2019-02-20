b1078f3c94e8aaa978f676292e19b3bbd3bbadd0
Patrick Martin <patrickmartin88@gmail.com>
2019-02-20 12:16:33 -0800

    Initial commit

---
+++ src/main/java/com/lambdaschool/javaordersqlite/JavaordersqliteApplication.java
@@ -0,0 +1,13 @@
+package com.lambdaschool.javaordersqlite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaordersqliteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaordersqliteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lambdaschool/javaordersqlite/JavaordersqliteApplicationTests.java
@@ -0,0 +1,16 @@
+package com.lambdaschool.javaordersqlite;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JavaordersqliteApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
